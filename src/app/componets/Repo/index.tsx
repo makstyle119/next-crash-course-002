94861c62104c9f57e0f768512dd69d7fcd971cc2
makstyle119 <moizprince786@gmail.com>
2023-06-13 15:11:41 +0500

    make repo and repoDir as client side component

--- src/app/componets/Repo/index.tsx
+++ src/app/componets/Repo/index.tsx
@@ -1,19 +1,71 @@
+// import { FaCodeBranch, FaEye, FaStar } from 'react-icons/fa';
+
+// async function fetchRepo(name: string) {
+// 	const response = await fetch(
+// 		`http://api.github.com/repos/makstyle119/${name}`,
+// 		{
+// 			next: {
+// 				revalidate: 60 * 60 * 24,
+// 			},
+// 		}
+// 	);
+// 	const repo = response.json();
+// 	return repo;
+// }
+// const Repo = async ({ name }: { name: string }) => {
+// 	const repo: any = await fetchRepo(name);
+// 	return (
+// 		<>
+// 			<h2>{repo?.name}</h2>
+// 			<p>{repo?.description}</p>
+// 			<div className='card-stats'>
+// 				<div className='card-stat'>
+// 					<FaStar />
+// 					<span>{repo?.stargazers_count}</span>
+// 				</div>
+// 				<div className='card-stat'>
+// 					<FaCodeBranch />
+// 					<span>{repo?.forks_count}</span>
+// 				</div>
+// 				<div className='card-stat'>
+// 					<FaEye />
+// 					<span>{repo?.watchers_count}</span>
+// 				</div>
+// 			</div>
+// 		</>
+// 	);
+// };
+
+// export default Repo;
+
+'use client';
+import React, { useEffect, useState } from 'react';
 import { FaCodeBranch, FaEye, FaStar } from 'react-icons/fa';
 
-async function fetchRepo(name: string) {
-	const response = await fetch(
-		`http://api.github.com/repos/makstyle119/${name}`,
-		{
-			next: {
-				revalidate: 60 * 60 * 24,
-			},
-		}
-	);
-	const repo = response.json();
-	return repo;
-}
-const Repo = async ({ name }: { name: string }) => {
-	const repo: any = await fetchRepo(name);
+const Repo = ({ name }: { name: string }) => {
+	const [repo, setRepo] = useState(null);
+
+	useEffect(() => {
+		const fetchRepo = async () => {
+			const response = await fetch(
+				`http://api.github.com/repos/makstyle119/${name}`,
+				{
+					next: {
+						revalidate: 60 * 60 * 24,
+					},
+				}
+			);
+			const repo = await response.json();
+			setRepo(repo);
+		};
+
+		fetchRepo();
+	}, []);
+
+	if (!repo) {
+		return <div>Detail loading...</div>;
+	}
+
 	return (
 		<>
 			<h2>{repo?.name}</h2>
--- src/app/componets/RepoDirs/index.tsx
+++ src/app/componets/RepoDirs/index.tsx
@@ -1,34 +1,78 @@
+// import Link from 'next/link';
+
+// async function fetchRepoContents(name: string) {
+// 	await new Promise((resolve) => setTimeout(resolve, 1000));
+
+// 	const response = await fetch(
+// 		`http://api.github.com/repos/makstyle119/${name}/contents`,
+// 		{
+// 			next: {
+// 				revalidate: 60 * 60 * 24,
+// 			},
+// 		}
+// 	);
+// 	const contents = response.json();
+// 	return contents;
+// }
+
+// const RepoDirs = async ({ name }: { name: string }) => {
+// 	const contents = await fetchRepoContents(name);
+// 	const dirs = contents.filter((content: any) => content.type === 'dir');
+// 	return (
+// 		<>
+// 			<h3>Directories</h3>
+// 			<ul>
+// 				{dirs.map((dir: any) => (
+// 					<li key={dir.path}>
+// 						<Link href={`/code/repos/${name}/${dir.path}`}>{dir.name}</Link>
+// 					</li>
+// 				))}
+// 			</ul>
+// 		</>
+// 	);
+// };
+
+// export default RepoDirs;
+
+'use client';
 import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
 
-async function fetchRepoContents(name: string) {
-	await new Promise((resolve) => setTimeout(resolve, 1000));
+const RepoDirs = ({ name }: { name: string }) => {
+	const [repoDirs, setRepoDirs] = useState([]);
 
-	const response = await fetch(
-		`http://api.github.com/repos/makstyle119/${name}/contents`,
-		{
-			next: {
-				revalidate: 60 * 60 * 24,
-			},
-		}
-	);
-	const contents = response.json();
-	return contents;
-}
+	useEffect(() => {
+		const fetchRepoDirs = async () => {
+			const response = await fetch(
+				`http://api.github.com/repos/makstyle119/${name}/contents`,
+				{
+					next: {
+						revalidate: 60 * 60 * 24,
+					},
+				}
+			);
+			const repo = await response.json();
+			setRepoDirs(repo);
+		};
 
-const RepoDirs = async ({ name }: { name: string }) => {
-	const contents = await fetchRepoContents(name);
-	const dirs = contents.filter((content: any) => content.type === 'dir');
+		fetchRepoDirs();
+	}, []);
+
+	if (!repoDirs) {
+		return <div>Loading Directories...</div>;
+	}
+
 	return (
-		<>
+		<div>
 			<h3>Directories</h3>
 			<ul>
-				{dirs.map((dir: any) => (
+				{repoDirs.map((dir: any) => (
 					<li key={dir.path}>
 						<Link href={`/code/repos/${name}/${dir.path}`}>{dir.name}</Link>
 					</li>
 				))}
 			</ul>
-		</>
+		</div>
 	);
 };
 
